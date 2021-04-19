01e817319647f57ec7432b94781d715354c69923
kobayashi9127 <bacchi.9127@gmail.com>
2021-04-19 11:30:42 +0000

    [編集]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'homes#top'
+  
+  post 'books' => 'books#create'
+  
+  resources :books, only: [:create, :index, :show, :update, :destroy, :edit]
+  
+  resources :homes, only: [:index]
+  
 end
